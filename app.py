94af5f81c26a7420daaa943a32bfb9feb49c5df8
Deepanshu Panwar <157171999+Deepanshu-Panwarr@users.noreply.github.com>
2026-06-13 00:24:40 +0530

    Fix typo in nltk download function

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import nltk
 
 nltk.download('punkt')
 nltk.download('punkt_tab')
-nlkt.download('stopwords')
+nltk.download('stopwords')
 
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
